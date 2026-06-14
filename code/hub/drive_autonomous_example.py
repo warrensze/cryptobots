dd5aa74857a7cf49e4379d2db692da558ca011a3
Earon Sze <zearon13@gmail.com>
2026-06-14 16:42:02 -0700

    6th degree polynomial for better accuracy

--- code/hub/drive_autonomous_example.py
+++ code/hub/drive_autonomous_example.py
@@ -26,8 +26,8 @@ try:
         # 2. Run polynomial math equation route 
         # Put the equation here - -0.318 + 0.0213x + -1.28E-05x^2 + -5.39E-08x^3 + 8.66E-11x^4 + -3.54E-14x^5
         #target_angle = -4.68 + -0.0871*x + 1.72E-03*x**2 + -4.64E-06*x**3 + 4.23E-09*x**4 + -1.25E-12*x**5
-        target_angle = -1 * (-19.5 + 0.942*x + -4.76E-03*x**2 + 7E-06*x**3 + -3.94E-09*x**4 + 7.61E-13*x**5)  # left then right curve
-        fit_R2 = 0.725  # Assuming a perfect fit for simplicity
+        target_angle = -1 * (3.78 + -0.488*x + 4.46E-03*x**2 + -1.45E-05*x**3 + 1.87E-08*x**4 + -1.03E-11*x**5 + 2.03E-15*x**6)  # left then right curve
+        fit_R2 = 0.789  # Assuming a perfect fit for simplicity
 
         # Add this in if you just want to drive straight during these distances
 #        if x <= 30 or x > 138:
