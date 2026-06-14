58a0f0dad19e6150e9c362105c32774ba679dcdc
Earon Sze <zearon13@gmail.com>
2026-06-14 16:33:14 -0700

    adjusted scale and direction

--- code/hub/drive_autonomous_example.py
+++ code/hub/drive_autonomous_example.py
@@ -1,34 +1,48 @@
+# Modify the target-angle equation to match your equation
+
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor
-from pybricks.parameters import Port
+from pybricks.parameters import Port, Direction
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
 
 hub = PrimeHub()
-left_motor = Motor(Port.A)
-right_motor = Motor(Port.B)
+left_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.F, Direction.CLOCKWISE)
+
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=114)
 
 robot.reset()
 hub.imu.reset_heading(0)
 
 base_speed = 150 
-proportional_gain = 2.0  
+proportional_gain = 8.0  
 
 try:
     while robot.distance() < 1000: # Drive for 1000mm (100cm)
         # 1. Convert mm to cm to prevent numbers from blowing up
-        x = robot.distance() / 10.0 
+        x = robot.distance() / 1.0
         
-        # 2. Run your polynomial math safely using small numbers
+        # 2. Run polynomial math equation route 
         # Put the equation here - -0.318 + 0.0213x + -1.28E-05x^2 + -5.39E-08x^3 + 8.66E-11x^4 + -3.54E-14x^5
-        target_angle = -0.318 + 0.0213*x + -1.28E-05*x**2 + -5.39E-08*x**3 + 8.66E-11*x**4 + -3.54E-14*x**5
-        
+        #target_angle = -4.68 + -0.0871*x + 1.72E-03*x**2 + -4.64E-06*x**3 + 4.23E-09*x**4 + -1.25E-12*x**5
+        target_angle = -1 * (-0.715 + -0.071*x + 2.09E-03*x**2 + -5.92E-06*x**3 + 5.58E-09*x**4 + -1.72E-12*x**5)  # left then right curve
+        fit_R2 = 0.987  # Assuming a perfect fit for simplicity
+
+        # Add this in if you just want to drive straight during these distances
+#        if x <= 30 or x > 138:
+#            target_angle = 0.0  # Drive straight at the beginning and end of the path
+#        else:
+        target_angle = target_angle * fit_R2  # Scale the target angle by the R² value to improve accuracy
+
         # 3. Calculate steering corrections
         current_angle = hub.imu.heading()
         error = target_angle - current_angle
         
         turn_rate = error * proportional_gain
+
+        print(round(x,1), round(target_angle,1), round(current_angle,1), round(error,2), round(turn_rate,2), sep=" |")
+
         robot.drive(base_speed, turn_rate)
         
         wait(10)
